f0ef2b6ef8328d9d3f9dcdf30a6cd9c1778e697a
Josh Calahan <jcalahan@gmail.com>
2011-11-02 18:18:10 -0500

    Added a basic service.

--- android-app/src/com/googlecode/tunnelk/TunnelkInitialActivity.java
+++ android-app/src/com/googlecode/tunnelk/TunnelkInitialActivity.java
@@ -3,12 +3,18 @@ package com.googlecode.tunnelk;
 import java.io.IOException;
 
 import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
 import android.content.res.AssetFileDescriptor;
 import android.content.res.AssetManager;
-import android.widget.TextView;
 import android.os.Bundle;
+import android.os.IBinder;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class TunnelkInitialActivity extends Activity
 {
@@ -52,10 +58,67 @@ public class TunnelkInitialActivity extends Activity
 
         message += "\n\nlength = " + Long.toString(fileSize);
         message += "\noffset = " + Long.toString(offset);
+
+        doBindService();
         
         setContentView(R.layout.main);
         TextView tv = (TextView) findViewById(R.id.solverOutput);
         tv.setHorizontallyScrolling(true);
         tv.setText(message);
     }
+    
+    @SuppressWarnings("unused")
+	private LocalService mBoundService;
+
+    private ServiceConnection mConnection = new ServiceConnection() {
+        public void onServiceConnected(ComponentName className, IBinder service) {
+            // This is called when the connection with the service has been
+            // established, giving us the service object we can use to
+            // interact with the service.  Because we have bound to a explicit
+            // service that we know is running in our own process, we can
+            // cast its IBinder to a concrete class and directly access it.
+            mBoundService = ((LocalService.LocalBinder)service).getService();
+
+            // Tell the user about this for our demo.
+            Toast.makeText(TunnelkInitialActivity.this, "onServiceConnected",
+                    Toast.LENGTH_SHORT).show();
+        }
+
+        public void onServiceDisconnected(ComponentName className) {
+            // This is called when the connection with the service has been
+            // unexpectedly disconnected -- that is, its process crashed.
+            // Because it is running in our same process, we should never
+            // see this happen.
+            mBoundService = null;
+            Toast.makeText(TunnelkInitialActivity.this, "onServiceDisconnected",
+                    Toast.LENGTH_SHORT).show();
+        }
+    };
+
+    boolean mIsBound = false;
+    
+    void doBindService() {
+        // Establish a connection with the service.  We use an explicit
+        // class name because we want a specific service implementation that
+        // we know will be running in our own process (and thus won't be
+        // supporting component replacement by other applications).
+        bindService(new Intent(TunnelkInitialActivity.this, 
+                LocalService.class), mConnection, Context.BIND_AUTO_CREATE);
+        mIsBound = true;
+    }
+
+    void doUnbindService() {
+        if (mIsBound) {
+            // Detach our existing connection.
+            unbindService(mConnection);
+            mIsBound = false;
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        doUnbindService();
+    }
+    
 }
