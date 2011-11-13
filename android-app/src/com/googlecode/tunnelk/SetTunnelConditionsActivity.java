66d50aeaed4fde058862ded41a055a41b8872e54
Brian Pittman <shweazel@gmail.com>
2011-11-13 11:37:27 -0600

    Use spinners instead of buttons to set tunnel conditions

--- android-app/src/com/googlecode/tunnelk/SetTunnelConditionsActivity.java
+++ android-app/src/com/googlecode/tunnelk/SetTunnelConditionsActivity.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class SetTunnelConditionsActivity extends Activity
 {
@@ -13,6 +15,18 @@ public class SetTunnelConditionsActivity extends Activity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.set_tunnel_conditions);
+
+        Spinner windSpeedSpinner = (Spinner) findViewById(R.id.windSpeedSpinner);
+        Spinner temperatureSpinner = (Spinner) findViewById(R.id.temperatureSpinner);
+        Spinner humiditySpinner = (Spinner) findViewById(R.id.humiditySpinner);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+                this, R.array.low_med_high_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        windSpeedSpinner.setAdapter(adapter);
+        temperatureSpinner.setAdapter(adapter);
+        humiditySpinner.setAdapter(adapter);
 
     }
     public void nextClicked(View v){
