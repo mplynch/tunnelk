981fe9df660d35487a632332fdece714ac713c70
Brian Pittman <shweazel@gmail.com>
2011-11-13 13:16:38 -0600

    Remove spurious import

--- android-app/src/com/googlecode/tunnelk/TunnelkInitialActivity.java
+++ android-app/src/com/googlecode/tunnelk/TunnelkInitialActivity.java
@@ -15,7 +15,6 @@ import android.os.Handler;
 import android.os.IBinder;
 import android.os.SystemClock;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
