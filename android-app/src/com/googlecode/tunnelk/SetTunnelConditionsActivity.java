fbf0fbc23b0e726d3086ad0dda6e98057d5710c0
Brian Pittman <shweazel@gmail.com>
2011-11-13 13:14:38 -0600

    Save/restore tunnel conditions to/from sharedPreferences

--- android-app/src/com/googlecode/tunnelk/SetTunnelConditionsActivity.java
+++ android-app/src/com/googlecode/tunnelk/SetTunnelConditionsActivity.java
@@ -1,7 +1,9 @@
 package com.googlecode.tunnelk;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
@@ -9,6 +11,10 @@ import android.widget.Spinner;
 
 public class SetTunnelConditionsActivity extends Activity
 {
+	private Spinner windSpeedSpinner;
+	private Spinner temperatureSpinner;
+	private Spinner humiditySpinner;
+
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
@@ -16,9 +22,9 @@ public class SetTunnelConditionsActivity extends Activity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.set_tunnel_conditions);
 
-        Spinner windSpeedSpinner = (Spinner) findViewById(R.id.windSpeedSpinner);
-        Spinner temperatureSpinner = (Spinner) findViewById(R.id.temperatureSpinner);
-        Spinner humiditySpinner = (Spinner) findViewById(R.id.humiditySpinner);
+        windSpeedSpinner = (Spinner) findViewById(R.id.windSpeedSpinner);
+        temperatureSpinner = (Spinner) findViewById(R.id.temperatureSpinner);
+        humiditySpinner = (Spinner) findViewById(R.id.humiditySpinner);
 
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
                 this, R.array.low_med_high_array, android.R.layout.simple_spinner_item);
@@ -33,5 +39,34 @@ public class SetTunnelConditionsActivity extends Activity
         Intent intent = new Intent();
         intent.setClass(this,SetAirfoilShapeActivity.class);
         startActivity(intent);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        Context context = getApplicationContext();
+        SharedPreferences p = context.getSharedPreferences("TunnelkPrefs", 0);
+        SharedPreferences.Editor e = p.edit();
+
+        e.putInt("@string/wind_speed", windSpeedSpinner.getSelectedItemPosition());
+        e.putInt("@string/temperature", temperatureSpinner.getSelectedItemPosition());
+        e.putInt("@string/humidity", humiditySpinner.getSelectedItemPosition());
+
+        e.commit();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Context context = getApplicationContext();
+        SharedPreferences p = context.getSharedPreferences("TunnelkPrefs", 0);
+
+        int windSpeedPos = p.getInt("@string/wind_speed",1);
+        int temperaturePos = p.getInt("@string/temperature",1);
+        int humidityPos = p.getInt("@string/humidity",1);
+
+        windSpeedSpinner.setSelection(windSpeedPos);
+        temperatureSpinner.setSelection(temperaturePos);
+        humiditySpinner.setSelection(humidityPos);
     }
 }
