61ad35346d188fcfce9d264673a4390cba7645fb
Brian Pittman <shweazel@gmail.com>
2011-12-04 16:03:01 -0600

    Go back to initial activity on "New Sim" button click

--- android-app/src/com/googlecode/tunnelk/ShowResultsActivity.java
+++ android-app/src/com/googlecode/tunnelk/ShowResultsActivity.java
@@ -1,7 +1,9 @@
 package com.googlecode.tunnelk;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class ShowResultsActivity extends Activity
 {
@@ -12,5 +14,11 @@ public class ShowResultsActivity extends Activity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.show_results);
 
+    }
+
+    public void newSimClicked(View v){
+        Intent intent = new Intent();
+        intent.setClass(this,TunnelkInitialActivity.class);
+        startActivity(intent);
     }
 }
