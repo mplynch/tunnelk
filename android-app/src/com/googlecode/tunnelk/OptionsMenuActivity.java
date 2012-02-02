bdbf53e572e4c28c5e10938baf061b35ce915189
Michael Lynch <mplynch@gmail.com>
2012-02-01 20:12:09 -0600

    Added option to start the exhibit over.  Removed menu from the SplashActivity.

--- android-app/src/com/googlecode/tunnelk/OptionsMenuActivity.java
+++ android-app/src/com/googlecode/tunnelk/OptionsMenuActivity.java
@@ -8,33 +8,41 @@ import android.view.MenuItem;
 
 public class OptionsMenuActivity extends Activity {
 
-    @Override
-    public boolean onPrepareOptionsMenu(Menu menu) {
-        super.onPrepareOptionsMenu(menu);
-        return true;
-    }
+	@Override
+	public boolean onPrepareOptionsMenu(Menu menu) {
+		super.onPrepareOptionsMenu(menu);
+		return true;
+	}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        super.onCreateOptionsMenu(menu);
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.options_menu, menu);
-        return true;
-    }
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		super.onCreateOptionsMenu(menu);
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.options_menu, menu);
+		return true;
+	}
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        Intent intent = new Intent();
-        switch (item.getItemId()) {
-            case R.id.virtual_home:
-                intent.setClass(this,TunnelkInitialActivity.class);
-                startActivity(intent);
-                return true;
-            case R.id.physical_home:
-                intent.setClass(this,PhysicalTunnelHMIActivity.class);
-                startActivity(intent);
-                return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		Intent intent = new Intent();
+
+		switch (item.getItemId()) {
+		case R.id.start_over:
+			intent.setClass(this, SplashActivity.class);
+			startActivity(intent);
+			return true;
+
+		case R.id.virtual_home:
+			intent.setClass(this, TunnelkInitialActivity.class);
+			startActivity(intent);
+			return true;
+
+		case R.id.physical_home:
+			intent.setClass(this, PhysicalTunnelHMIActivity.class);
+			startActivity(intent);
+			return true;
+		}
+
+		return super.onOptionsItemSelected(item);
+	}
 }
--- android-app/src/com/googlecode/tunnelk/SplashActivity.java
+++ android-app/src/com/googlecode/tunnelk/SplashActivity.java
@@ -1,12 +1,13 @@
 package com.googlecode.tunnelk;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 
-public class SplashActivity extends OptionsMenuActivity {
+public class SplashActivity extends Activity {
 	
 	/** Called when the activity is first created. */
 	@Override
