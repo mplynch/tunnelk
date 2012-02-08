42e51b4e2683f051c183fa983bdfa49038fca877
chrisandsusan3587 <chrisandsusan3587@gmail.com>
2012-02-08 04:40:06 +0000

    Adding Stepper Motor example that runs the stepper motor in one direction for two seconds, then reverses its direction for two seconds.

---
+++ prototypes/Stepper_Motor_Prototype.ino
@@ -0,0 +1,22 @@
+int StepOutputPin = 10;
+int DirectionPin = 7;
+int counter = 0;
+void setup() 
+{
+  // put your setup code here, to run once:
+  pinMode(StepOutputPin, OUTPUT);
+  pinMode(DirectionPin, OUTPUT);  
+}
+
+void loop() 
+{
+  // put your main code here, to run repeatedly: 
+  while(1)
+  {
+    analogWrite(StepOutputPin, 127);
+    digitalWrite(DirectionPin, LOW);
+    delay(2000);
+    digitalWrite(DirectionPin, HIGH);
+    delay(2000);
+  }
+}
